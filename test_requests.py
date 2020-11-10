78a24fdddcec290a59e1dd9646b38441af7ac9f2
victorrguez <victor.rodriguez@cleverppc.com>
2020-11-10 21:47:01 +0000

    test: modified tsv file without extension name

--- test_requests.py
+++ test_requests.py
@@ -7,28 +7,28 @@ TEST_IP = 'http://0.0.0.0:5000'
 
 class TestRequests(unittest.TestCase):
     def test_csv_with_extension(self):
-        url = 'https://www.indiandcold.com/media/photo_export.csv'
+        url = 'https://raw.githubusercontent.com/vitururu/photoslurp/master/test_files/photo_export.csv'
         response = requests.get(TEST_IP, params = {'url':url})
         response = json.loads(response.content)
         correct_response = {'url': url, 'format': 'csv'}
         self.assertEqual(response, correct_response)
 
     def test_tsv_with_extension(self):
-        url = 'https://www.dropbox.com/s/6wgcp2wyl2p71q8/example_feed.tsv'
+        url = 'https://raw.githubusercontent.com/vitururu/photoslurp/master/test_files/example_feed.tsv'
         response = requests.get(TEST_IP, data = {'url':url})
         response = json.loads(response.content)
         correct_response = {'url': url, 'format': 'tsv'}
         self.assertEqual(response, correct_response)
 
     def test_xml_with_extension(self):
-        url = 'https://www.w3schools.com/xml/note.xml'
+        url = 'https://raw.githubusercontent.com/vitururu/photoslurp/master/test_files/test.xml'
         response = requests.get(TEST_IP, data = {'url':url})
         response = json.loads(response.content)
         correct_response = {'url': url, 'format': 'xml'}
         self.assertEqual(response, correct_response)
 
     def test_csv_without_extension(self):
-        url = 'https://drive.google.com/file/d/10APUcJCrnkCTk-mIdBMmnxSYFKl06reR/view?usp=sharing'
+        url = 'https://raw.githubusercontent.com/vitururu/photoslurp/master/test_files/photo_export_csv'
         response = requests.get(TEST_IP, data = {'url':url})
         response = json.loads(response.content)
         correct_response = {'url': url, 'format': 'csv'}
@@ -42,14 +42,14 @@ class TestRequests(unittest.TestCase):
         self.assertEqual(response, correct_response)
 
     def test_xml_without_extension(self):
-        url = 'https://drive.google.com/file/d/1bT5v6hNWma8e6iR98ardoGYBhQvCZFnO/view?usp=sharing'
+        url = 'https://raw.githubusercontent.com/vitururu/photoslurp/master/test_files/test_xml'
         response = requests.get(TEST_IP, data = {'url':url})
         response = json.loads(response.content)
         correct_response = {'url': url, 'format': 'xml'}
         self.assertEqual(response, correct_response)
 
     def test_return_error(self):
-        url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
+        url = 'https://raw.githubusercontent.com/vitururu/photoslurp/master/test_files/favicon.ico'
         response = requests.get(TEST_IP, data = {'url':url})
         response = json.loads(response.content)
         correct_response = {'url': url, "error": "File format unknown or not recognizable."}
